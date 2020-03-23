76fa0eac0f8b9f64a729c2e0ec081afb9c78e606
MartínTrujillo Bustamante <martin.trujillo1105@gmail.com>
2020-03-23 12:27:52 -0500

    ewrwer

---
+++ menu.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse navbar-dark bg-dark fixed-top" style="margin-bottom:0px">
+	<div class="container">
+		<div class="navbar-collapse collapse" id="navbarResponsive">
+			<ul class="nav navbar-nav pull-right mainNav">
+				<li class="active"><a href="index.php">INICIO</a></li>
+				<li><a href="contactenos.php">CONTACTENOS</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<!-- Page Header -->
+<header class="masthead" style='background-image: url("assets/img/home-bg.jpg")'>
+  <div class="overlay"></div>
+  <div class="container">
+	<div class="row">
+	  <div class="col-lg-8 col-md-10 mx-auto">
+		<div class="site-heading">
+		  <h1>CECCOS - Circulo de Estudios</h1>
+		  <span class="subheading">Capacitacion y comunicacion social</span>
+		</div>
+	  </div>
+	</div>
+  </div>
+</header>
